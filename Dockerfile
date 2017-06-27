84fd5fd7f0c3e4782d4f8ea5359ee95ea62d17b7
pierreozoux <pierre@ozoux.net>
2017-06-27 12:15:19 +0100

    Use submodule for reveal.js

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:0.10-onbuild
-
-RUN npm install -g grunt
-
-ENTRYPOINT ["grunt"]
-CMD ["serve"]
-
-VOLUME /usr/src/app
--- reveal_conf.js
+++ reveal_conf.js
@@ -12,21 +12,13 @@ Reveal.initialize({
   history: true,
   center: true,
   transition: 'slide', // none/fade/slide/convex/concave/zoom
-  multiplex: {
-    secret: null, // null so the clients do not have control of the master presentation
-    id: 'ef1fbc8686178b0e', // id, obtained from socket.io server
-    url: 'https://presentations.pierre-o.fr:443' // Location of socket.io server
-  },
   // Optional reveal.js plugins
   dependencies: [
-    { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-    { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-    { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-    { src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-    { src: 'plugin/zoom-js/zoom.js', async: true },
-    { src: 'plugin/notes/notes.js', async: true },
-    { src: '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js', async: true },
-    { src: 'plugin/multiplex/client.js', async: true }
+    { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+    { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true },
+    { src: 'reveal.js/plugin/notes/notes.js', async: true }
   ]
 });
-
